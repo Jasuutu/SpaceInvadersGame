390a9382ea7b186764a9f0b983b12cd7002c547e
alriightyman <alriightyman@gmail.com>
2017-06-16 22:59:59 -0400

    Created PlayerController script and Projectile script. Also, created the base gameobjects for the player and projectile.

---
+++ Space invader Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectile;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Move();
+
+        Fire();
+	}
+
+    /// <summary>
+    /// Moves the player left/right
+    /// </summary>
+    void Move()
+    {
+        // am I going left... or right..?
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // set the speed
+        float speed = horizontal * moveSpeed * Time.deltaTime;
+
+        // add speed to x position
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y, 0f);
+    }
+
+    /// <summary>
+    /// Fires a projectile
+    /// </summary>
+    void Fire()
+    {      
+        if(Input.GetButtonDown("Fire1"))
+        {  
+            // create the projectile
+            Instantiate(projectile, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ Space invader Game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float moveSpeed = 10f; 
+	// Use this for initialization
+	void Awake ()
+    {
+        // kill me, just not right this second...
+        Destroy(gameObject, 2f);	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // just movin' on up :)
+        transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime, 0f);
+	} 
+}
